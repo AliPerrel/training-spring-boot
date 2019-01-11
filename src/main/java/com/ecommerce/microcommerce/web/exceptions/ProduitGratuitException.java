dd830cff49f53fb35af5d134e19808bd6c2d8a07
Ali Perrel <ali.perrel@gmail.com>
2019-01-11 14:10:20 +0100

    Partie 3 : Validation du prix de vente --> correction mineure

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
@@ -3,7 +3,7 @@ package com.ecommerce.microcommerce.web.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
 public class ProduitGratuitException extends RuntimeException {
 
     public ProduitGratuitException(String s) {
